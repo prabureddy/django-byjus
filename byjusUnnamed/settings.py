65340b1cf941bc922e7c08fc1349ef5b4d111aab
Bhargava Prabu Reddy <44055337+prabureddy@users.noreply.github.com>
2020-07-05 19:41:12 +0530

    c

--- byjusUnnamed/settings.py
+++ byjusUnnamed/settings.py
@@ -21,9 +21,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 # Quick-start development settings - unsuitable for production
